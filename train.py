0332abe17775421f30ae9d74397117053cce01ba
maxqiqq <116487542+maxqiqq@users.noreply.github.com>
2024-03-13 21:39:56 +0800

    Update train.py

--- train.py
+++ train.py
@@ -173,10 +173,7 @@ if __name__ == '__main__':
                         out = translator(inp, mask)
 
                     if idx % opt.save_checkpoint == 0 and idx > 0:
-                        out_numpy = out.detach().cpu().numpy()
-                        out_numpy = out_numpy.permute(1, 2, 0)
-                        out_numpy = np.clip(out_numpy, 0, 1)
-                        out_image = Image.fromarray((out_numpy * 255).astype(np.uint8))
+                        out_img = transforms.ToPILImage()(out[0])
                         img_name = os.path.splitext(os.path.basename(B_img))[0]
                         wandb.log({"prediction_epoch{}_{}".format(epoch, img_name): [wandb.Image(out_image)]})
                                     
